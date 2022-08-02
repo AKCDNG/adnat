7affb3605751e298e870e6058d86b6662c7deff0
Anish <anishkakaiya@outlook.com>
2022-08-02 22:38:47 +0100

    add create method

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to home_path
+    else
+      flash[:alert] = "Cannot login. Please try again."
+      redirect_to login_path
+    end
+  end
+end
