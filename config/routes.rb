447a32d99138dd93bf7b06179bf42495fb3009c7
Anish <anishkakaiya@outlook.com>
2022-08-02 23:24:41 +0100

    add home route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   resources :users, except: [:new]
+
+  get "home", to: "home#view"
 end
