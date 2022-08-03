a030a18f7e5b0aa8711f224fa43d88180d6ee6b2
Anish <anishkakaiya@outlook.com>
2022-08-03 22:38:50 +0100

    add tables to db

--- db/migrate/20220803213510_create_shifts.rb
+++ db/migrate/20220803213510_create_shifts.rb
@@ -1,7 +1,10 @@
 class CreateShifts < ActiveRecord::Migration[7.0]
   def change
     create_table :shifts do |t|
-
+        t.belongs_to :user, null: false, foreign_key: true
+        t.datetime :start, null: false
+        t.datetime :finish, null: false
+        t.integer :break_length, null: false
       t.timestamps
     end
   end
