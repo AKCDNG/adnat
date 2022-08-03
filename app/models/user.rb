02f078060d099e22c6b1c6778d740acd5cbfdf54
Anish <anishkakaiya@outlook.com>
2022-08-03 12:35:27 +0100

    add regex formatting for the email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, length: { minimum: 6}
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }, uniqueness: true
 end
