1413d7652b728a9eede266ba32b58e0380b341e1
Anish <anishkakaiya@outlook.com>
2022-08-03 22:43:49 +0100

    add regex for formatting of number

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,5 +3,6 @@ class Organisation < ApplicationRecord
 
   validates :name, presence :true
   validates :hourly_pay_rate, numericality: {greater_than: -1}
+  validates :hourly_pay_rate, format: { with: /\A\d+(?:\.\d{0,2})?\z/, message: "Pay must have 2 decimal points. E.g: £2.99"}
 
 end
