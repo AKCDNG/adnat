da5a40efca2754713e6981b0203a3f197e772365
Anish <anishkakaiya@outlook.com>
2022-08-03 22:47:13 +0100

    add validations for shift, experimenting with date and time validations

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,7 @@
 class Shift < ApplicationRecord
   belongs_to :user
+
+  validates :break_length, numericality: {only_integers: true}
+  validates_datetime :start, :finish
+  validates_date :start, on_or_before: :today
 end
