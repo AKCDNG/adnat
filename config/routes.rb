8532a65c5336a8401201fc71bf63304c58737a74
Anish <anishkakaiya@outlook.com>
2022-08-03 23:11:34 +0100

    add routes for joining and leaving an org

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
   resources :users, except: [:new]
+  resources :organisations, except: [:index, :show, :new]
 
   get "home", to: "home#view"
+  post '/join', to: 'memberships#create'
+  delete '/leave', to: 'memberships#destroy'
 end
