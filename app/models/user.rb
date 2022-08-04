58bc4f0f01d2209c96aed2af914db653017115c6
Anish <anishkakaiya@outlook.com>
2022-08-04 21:58:31 +0100

    refactor user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ApplicationRecord
   belongs_to :organisation, optional: true
-  has_many :shifts, through: :organisation
+  has_many :shifts
   
   has_secure_password
 
-  validates :password, length: { minimum: 6}
+  validates :password, length: { minimum: 6 }
   validates :name, presence: true
-  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
 end
  
