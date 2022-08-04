dd1f35c025f77e173edf691092e97bd73b9edea3
Anish <anishkakaiya@outlook.com>
2022-08-04 21:55:13 +0100

    refactor org model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,8 +1,9 @@
+
 class Organisation < ApplicationRecord
   has_many :users
 
-  validates :name, presence: :true
-  validates :hourly_pay_rate, numericality: {greater_than: -1}
-  validates :hourly_pay_rate, format: { with: /\A\d+(?:\.\d{0,2})?\z/, message: "Pay must have 2 decimal points. E.g: £2.99"}
-
+  validates :name, presence: true
+  validates :hourly_rate, numericality: { greater_than: -1 }
+  validates :hourly_rate, format: { with: /\A\d+(?:\.\d{0,2})?\z/,
+   message: "must have two decimal points" }
 end
