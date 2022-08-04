3268733f685b6d2986fc2695cb42b377196faefd
Anish <anishkakaiya@outlook.com>
2022-08-04 16:16:59 +0100

    fix syntax error with missing colon

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,7 +1,7 @@
 class Organisation < ApplicationRecord
   has_many :users
 
-  validates :name, presence :true
+  validates :name, presence: :true
   validates :hourly_pay_rate, numericality: {greater_than: -1}
   validates :hourly_pay_rate, format: { with: /\A\d+(?:\.\d{0,2})?\z/, message: "Pay must have 2 decimal points. E.g: £2.99"}
 
