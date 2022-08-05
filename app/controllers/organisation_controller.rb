1a3ee25114be447d558b4b3e24f18f3e78637b7d
Anish <anishkakaiya@outlook.com>
2022-08-05 17:19:01 +0100

    add create method

--- app/controllers/organisation_controller.rb
+++ app/controllers/organisation_controller.rb
@@ -1,9 +1,14 @@
 class OrganisationController < ApplicationController
+  before_action :set_organisation, except: [:create]
 
-  def index
-    @organisations = Organisation.all
-    @organisation = Organisation.new
+  def create
+    @organisation = Organisation.new(organisation_params)
+    if @organisation.save
+      redirect_to home_path
+    else
+      @organisations = Organisation.all 
+      @user = current_user
+      render "home/non_member_show" 
+    end
   end
-
-  
-end
+end 
