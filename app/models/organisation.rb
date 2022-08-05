0558545e84a468c8c866e1379870811177b8b2d7
Anish <anishkakaiya@outlook.com>
2022-08-05 15:34:29 +0100

    correct name of field in db for orgs

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,7 +3,7 @@ class Organisation < ApplicationRecord
   has_many :users
 
   validates :name, presence: true
-  validates :hourly_rate, numericality: { greater_than: -1 }
-  validates :hourly_rate, format: { with: /\A\d+(?:\.\d{0,2})?\z/,
+  validates :hourly_pay_rate, numericality: { greater_than: -1 }
+  validates :hourly_pay_rate, format: { with: /\A\d+(?:\.\d{0,2})?\z/,
    message: "must have two decimal points" }
 end
