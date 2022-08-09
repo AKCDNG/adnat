c14864ba24912c3c637aaa6161a16bce12c2a6c7
Anish <anishkakaiya@outlook.com>
2022-08-09 19:16:25 +0100

    add attr accessors

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,11 +1,10 @@
 class Shift < ApplicationRecord
+  attr_accessor :date, :start, :finish, :break_length
   belongs_to :organisation
   belongs_to :user
-
-  
   validates :start, presence: true
   validates :finish, presence: :true
+  validates_datetime :start
+  validates_datetime :finish
   validates :break_length, numericality: {only_integers: true}
-  validates_datetime :start, :finish
-  validates_date :start, on_or_before: :today
 end
